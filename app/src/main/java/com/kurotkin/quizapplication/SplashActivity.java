e303f85ee6fc3a1d5dfa755aaadb94c41a6d5622
kurotkin <kurotkin@users.noreply.github.com>
2018-12-12 21:58:45 +0300

    Added animation on splash activity

---
+++ app/src/main/java/com/kurotkin/quizapplication/SplashActivity.java
@@ -0,0 +1,61 @@
+package com.kurotkin.quizapplication;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Animation animation;
+    private ProgressBar progressBar;
+    private ConstraintLayout layout;
+
+    private static final int SPLASH_DURACTION = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        progressBar = findViewById(R.id.progressBar);
+        layout = findViewById(R.id.splash_layout);
+        imageView = findViewById(R.id.ivIcon);
+        animation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initFunc();
+    }
+
+    private void initFunc(){
+        layout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                progressBar.setVisibility(View.GONE);
+                imageView.startAnimation(animation);
+                animation.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        }, SPLASH_DURACTION);
+    }
+}
